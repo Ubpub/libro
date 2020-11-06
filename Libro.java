7ca6e7608b41f82e4d7a2723f58645d879741660
Cecilia Llamazares Lopez <ceciliallamazares99@gmail.com>
2020-11-06 09:20:27 +0100

    1847.03 Añadido el atributo numeroPaginas y getter para éste

--- Libro.java
+++ Libro.java
@@ -12,14 +12,16 @@ public class Libro {
 
     private String autor;
     private String titulo;
+    private int numeroPaginas;
 
     /**
      * Fija el autor y el titulo del libro a los dados como parametro
      */
-    public Libro(String autorLibro, String tituloLibro)
+    public Libro(String autorLibro, String tituloLibro, int numeroDePaginas)
     {
         autor = autorLibro;
         titulo = tituloLibro;
+        numeroPaginas = numeroDePaginas;
     }
     
     /**
@@ -36,11 +38,24 @@ public class Libro {
         return titulo;
     }
     
+    /**
+     * Imprime el nombre del autor
+     */
     public void imprimeAutor() {
         System.out.println("El autor es " + autor + ".");
     }
     
+    /**
+     * Imprime el título del libro
+     */
     public void imprimeTitulo() {
         System.out.println("El título del libro es " + titulo + ".");
+    }
+    
+    /**
+     * Devuelve el número de páginas del libro
+     */
+    public int getNumeroPaginas() {
+        return numeroPaginas;
     }
 }
